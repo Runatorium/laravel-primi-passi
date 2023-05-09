e172869bbd53b4edc78b85c09a63256ae08131ee
Runatorium <stefano.alesci1@gmail.com>
2023-05-09 16:34:20 +0200

    Passaggio dati + display dinamico di "$info"

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $info = [
+        'users' => [
+            'Stefano',
+            'Michele',
+            'Daniele',
+            'Giovanni'
+        ]
+    ];
+
+
+
+
+
+    return view('myHomepage', $info);
 });
